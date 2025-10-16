7e586c34efeeb1fe57ffb8133a5d32d170e53519
kolesnikovak <katyamiller36@gmail.com>
2025-10-15 19:33:56 -0500

    .

--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -1,12 +1,12 @@
 import {test, expect} from '@playwright/test';
 import {faker} from '@faker-js/faker';
 import {z} from 'zod';
+import { getAPI } from '../utils/apiCallHelper';
 
 test.describe('User API Tests', () => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
 
-    test ('Create User', async ({request}) => {
-        const newCreateUserRequestBody = {
+    const newCreateUserRequestBody = {
             id: 345238,
             username: "TestUserNameKatya",
             firstName: faker.person.firstName(),
@@ -16,28 +16,16 @@ test.describe('User API Tests', () => {
             phone: faker.phone.number(),
             userStatus: 0
         }
-        const createUserResponse = await request.post(`${BASE_URL}/user`, {
-            data: newCreateUserRequestBody
-        });
-        expect(createUserResponse.status()).toBe(200);
-        
+
 
         const expectedResponseSchema = z.object({
             code: z.literal(200),
             type: z.literal("unknown"),
             message: z.literal(newCreateUserRequestBody.id.toString()),
         });
-        
-        const actualUserResponseBody = await createUserResponse.json();
-        expectedResponseSchema.parse(actualUserResponseBody);
 
-    });
-    test ('Get User by Username', async ({request}) => {
-        const username = "TestUserNameKatya";
-        const getUserResponse = await request.get(`${BASE_URL}/user/${username}`);
-        expect(getUserResponse.status()).toBe(200);
 
-        const expectedGetUserResponseSchema = z.object({
+         const expectedGetUserResponseSchema = z.object({
             id: z.number(),
             username: z.string(),
             firstName: z.string(),
@@ -48,7 +36,29 @@ test.describe('User API Tests', () => {
             userStatus: z.number(),
         });
 
-        const actualGetUserResponseBody = await getUserResponse.json();
-        expectedGetUserResponseSchema.parse(actualGetUserResponseBody);
+
+
+
+    test ('Create User', async ({request}) => {
+        
+        const createUserResponse = await request.post(`${BASE_URL}/user`, {
+            data: newCreateUserRequestBody
+        });
+        expect(createUserResponse.status()).toBe(200);
+    
+        
+        const actualUserResponseBody = await createUserResponse.json();
+        expectedResponseSchema.parse(actualUserResponseBody);
+
+    });
+    test ('Get User by Username', async ({request}) => {
+        const username = "TestUserNameKatya";
+
+        await getAPI(
+            request,
+            `${BASE_URL}/user/${username}`,
+            200, 
+            expectedGetUserResponseSchema
+        );
     });
 });
