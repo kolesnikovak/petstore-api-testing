0c5faf08ff348ceeab1370705257d7288740da74
kolesnikovak <katyamiller36@gmail.com>
2025-10-15 19:44:54 -0500

    updated code with apiCallHelper

--- tests/userAPIs.spec.ts
+++ tests/userAPIs.spec.ts
@@ -1,10 +1,11 @@
 import {test, expect} from '@playwright/test';
 import {faker} from '@faker-js/faker';
 import {z} from 'zod';
-import { getAPI } from '../utils/apiCallHelper';
+import { deleteAPI, getAPI, postAPI } from '../utils/apiCallHelper';
 
 test.describe('User API Tests', () => {
     const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
+    const userName = "TestUserNameKatya";
 
     const newCreateUserRequestBody = {
             id: 345238,
@@ -27,7 +28,7 @@ test.describe('User API Tests', () => {
 
          const expectedGetUserResponseSchema = z.object({
             id: z.number(),
-            username: z.string(),
+            username: z.literal(userName),
             firstName: z.string(),
             lastName: z.string(),
             email: z.string().email(),
@@ -36,29 +37,41 @@ test.describe('User API Tests', () => {
             userStatus: z.number(),
         });
 
+            const expectedDeleteUserResponseSchema = z.object ({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.literal(userName)
+    });
 
 
 
     test ('Create User', async ({request}) => {
         
-        const createUserResponse = await request.post(`${BASE_URL}/user`, {
-            data: newCreateUserRequestBody
-        });
-        expect(createUserResponse.status()).toBe(200);
-    
-        
-        const actualUserResponseBody = await createUserResponse.json();
-        expectedResponseSchema.parse(actualUserResponseBody);
+      await postAPI(
+            request,
+            `${BASE_URL}/user`,
+            newCreateUserRequestBody,
+            200, 
+            expectedResponseSchema
+        );
 
     });
     test ('Get User by Username', async ({request}) => {
-        const username = "TestUserNameKatya";
 
         await getAPI(
             request,
-            `${BASE_URL}/user/${username}`,
+            `${BASE_URL}/user/${userName}`,
             200, 
             expectedGetUserResponseSchema
+        );
+    });
+
+    test ('Delete User by Username', async ({request}) => {
+        await deleteAPI(
+            request,
+            `${BASE_URL}/user/${userName}`,
+            200, 
+            expectedDeleteUserResponseSchema
         );
     });
 });
