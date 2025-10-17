9c67ed4498d0c0d4dddfa08d6705ef22aeb4c0e6
kolesnikovak <katyamiller36@gmail.com>
2025-10-16 20:03:18 -0500

    user creation, login, logout and delete api tests

---
+++ tests/userLoginLogout.spec.ts
@@ -0,0 +1,78 @@
+import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
+import { z } from 'zod';
+import { deleteAPI, getAPI, postAPI, putAPI } from '../utils/apiCallHelper';
+
+test.describe('User API Tests', () => {
+    const BASE_URL = `${process.env.BASE_URL}${process.env.API_VERSION}`;
+    const userName = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+
+    const newCreateUserRequestBody = {
+        id: 345238,
+        username: userName,
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        password: password,
+        phone: faker.phone.number(),
+        userStatus: 0
+    }
+    const expectedCreateUserResponseSchema = z.object({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.literal(newCreateUserRequestBody.id.toString()),
+    });
+
+    const expectedLoginResponseSchema = z.object({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.string().regex(/^logged in user session:\d+$/) // regex to match session ID format
+    });
+
+    const expectedLogoutResponseSchema = z.object({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.literal("ok")
+    });
+
+    const deleteUserResponseSchema = z.object({
+        code: z.literal(200),
+        type: z.literal("unknown"),
+        message: z.string()
+    });
+
+    test('User Createion, Login, Logout and Delete', async ({ request }) => {
+
+        await postAPI(
+            request,
+            `${BASE_URL}/user`,
+            newCreateUserRequestBody,
+            200,
+            expectedCreateUserResponseSchema
+        );
+
+        await getAPI(
+            request,
+            `${BASE_URL}/user/login`,
+            200,
+            expectedLoginResponseSchema,
+            { username: userName, password: password }
+        );
+
+        // User Logout
+        await getAPI(
+            request,
+            `${BASE_URL}/user/logout`,
+            200,
+            expectedLogoutResponseSchema
+        );
+
+        await deleteAPI(
+            request,
+            `${BASE_URL}/user/${userName}`,
+            200,
+            deleteUserResponseSchema
+        );
+    });
+});
